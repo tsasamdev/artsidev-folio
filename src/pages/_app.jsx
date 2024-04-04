ccebd7f9bf72a52b76633e04e978d2b8adfd2028
artsidev <tsiory.arisoa@gmail.com>
2024-04-04 16:26:50 +0200

    style: removed semicolon from style

--- src/pages/_app.jsx
+++ src/pages/_app.jsx
@@ -21,7 +21,7 @@ export default function App({ Component, pageProps, router }) {
 
   return (
     <>
-      <div className="fixed inset-0 flex justify-center sm:px-8" style={{background: 'radial-gradient(600px at 1179px 186px, rgba(29, 78, 216, 0.15), transparent 80%);'}}>
+      <div className="fixed inset-0 flex justify-center sm:px-8" style={{background: 'radial-gradient(600px at 1179px 186px, rgba(29, 78, 216, 0.15), transparent 80%)'}}>
         <div className="pointer-events-none fixed inset-0 z-30 transition duration-300 lg:absolute flex w-full max-w-7xl lg:px-8">
           <div className="w-full"/>
         </div>
